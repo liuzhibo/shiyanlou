f1af1237579649b088ec6602019ea99339124f6c
liuzhibo <liuzhibo@vip.qq.com>
2018-02-11 13:56:36 +0800

    挑战3 全用mongo

--- week2/challenge3/news/app.py
+++ week2/challenge3/news/app.py
@@ -1,73 +1,102 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin.env python3
+
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask_mongoengine import MongoEngine
 from datetime import datetime
 
 app = Flask(__name__)
-app.config[
-    'SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Aa111111@localhost/shilanlou'
-db = SQLAlchemy(app)
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'liuzhibo',
+    'host': '127.0.0.1',
+    'port': 27017
+}
+
+db = MongoEngine(app)
 
 
-class File(db.Model):
-    __tablename__ = 'files'
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), unique=True)
-    created_time = db.Column(db.DateTime)
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
-    category = db.relationship('Category', uselist=False)
-    content = db.Column(db.Text)
-
-    def __init__(self, title, created_time, category, content):
-        self.title = title
-        self.created_time = created_time
-        self.category = category
-        self.content = content
-
-    def __repr__(self):
-        return '<Flie %r>' % self.title
+class Files(db.Document):
+    meta = {
+        'collection': 'files',
+        'ordering': ['-create_at'],
+        'strict': False,
+    }
+    file_id = db.IntField()
+    title = db.StringField()
+    category = db.IntField()
+    tag = db.ListField()
+    content = db.StringField()
+    create_time = db.DateTimeField(default=datetime.now)
 
 
-class Category(db.Model):
-    __tablename__ = 'categories'
+class Category(db.Document):
+    meta = {
+        'collection': 'category',
+        'ordering': ['-create_at'],
+        'strict': False,
+    }
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
-    files = db.relationship('File')
-
-    def __init__(self, name):
-        self.name = name
-
-    def __repr__(self):
-        return '<Category %r>' % self.name
+    cid = db.IntField()
+    category = db.StringField()
 
 
-def insert_datas():
-    java = Category('Java')
-    python = Category('Python')
-    file1 = File('Hello Java', datetime.utcnow(), java, 'File Content - Java is cool!')
-    file2 = File('Hello Python', datetime.utcnow(), python, 'File Content - Python is cool!')
-    db.session.add(java)
-    db.session.add(python)
-    db.session.add(file1)
-    db.session.add(file2)
-    db.session.commit()
+class Tag(db.Document):
+    meta = {
+        'collection': 'tag',
+        'ordering': ['-create_at'],
+        'strict': False,
+    }
 
-@app.route('/')
+    tid = db.IntField()
+    tag = db.StringField()
+
+
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', files=File.query.all())
+    files = Files.objects().all()
+    tlist = []
+    flist = []
+    for f in files:
+        for t in f['tag']:
+            tlist.append(Tag.objects(tid=t)[0]['tag'])
+        flist.append([f, tlist, Category.objects(
+            cid=f['category'])[0]['category']])
+        tlist = []
+    # print(flist)
+    return render_template('index.html', files=flist)
 
 
-@app.route('/files/<int:file_id>')
+@app.route('/file/<int:file_id>')
 def file(file_id):
-    file_item = File.query.get_or_404(file_id)
-    return render_template('file.html', file_item=file_item)
+
+    files = Files.objects(file_id=file_id)
+    print(files[0]['create_time'])
+    return render_template('file.html', files=files[0])
 
 
-@app.errorhandler(404)
-def not_found(error):
-    return render_template('404.html'), 404
+@app.route('/add')
+def add():
+    f1 = Files(file_id=1, title='HI first !', category=1,
+               tag=[1], content='first content')
+    f2 = Files(file_id=2, title='HI second !', category=2,
+               tag=[2], content='second content')
+    f3 = Files(file_id=3, title='HI third !', category=2,
+               tag=[1, 2], content='third content')
+    c1 = Category(cid=1, category='category_1')
+    c2 = Category(cid=2, category='category_2')
+    t1 = Tag(tid=1, tag='tag_1')
+    t2 = Tag(tid=2, tag='tag_2')
+
+    f1.save()
+    f2.save()
+    f3.save()
+    c1.save()
+    c2.save()
+    t1.save()
+    t2.save()
+
+    return "<p>add succssfully! <a href='/'>Home</a></p>"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
