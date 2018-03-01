290621bf51b412f5ccfcf163ce2156154a311fd6
liuzhibo <liuzhibo@vip.qq.com>
2018-03-01 17:46:05 +0800

    实验18

--- week4/shiyanlou/shiyanlou/items.py
+++ week4/shiyanlou/shiyanlou/items.py
@@ -8,7 +8,12 @@
 import scrapy
 
 
-class ShiyanlouItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class CourseItem(scrapy.Item):
+    """定义 Item 非常简单，只需要继承 scrapy.Item 类，将每个要爬取
+    的数据声明为 scrapy.Field()。下面的代码我们每个课程要爬取的 4
+    个数据。
+    """
+    name = scrapy.Field()
+    description = scrapy.Field()
+    type = scrapy.Field()
+    students = scrapy.Field()
--- week4/shiyanlou/shiyanlou/pipelines.py
+++ week4/shiyanlou/shiyanlou/pipelines.py
@@ -1,11 +1,36 @@
-# -*- coding: utf-8 -*-
-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from sqlalchemy.orm import sessionmaker
+from shiyanlou.models import Course, engine
 
 
 class ShiyanlouPipeline(object):
+
     def process_item(self, item, spider):
+        # 提取的学习人数是字符串，把它转换成 int
+        item['students'] = int(item['students'])
+        # 根据 item 创建 Course Model 对象并添加到 session
+        # item 可以当成字典来用，所以也可以使用字典解构, 相当于
+        # Course(
+        #     name=item['name'],
+        #     type=item['type'],
+        #     ...,
+        # )
+        # if item['students'] < 1000:
+        #     # 对于不需要的 item，raise DropItem 异常
+        #     raise DropItem('Course students less than 1000.')
+        # else:
+        #     self.session.add(Course(**item))
+
+        self.session.add(Course(**item))
         return item
+
+    def open_spider(self, spider):
+        """ 在爬虫被开启的时候，创建数据库 session
+        """
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
+
+    def close_spider(self, spider):
+        """ 爬虫关闭后，提交 session 然后关闭 session
+        """
+        self.session.commit()
+        self.session.close()
