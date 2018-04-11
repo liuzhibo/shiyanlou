ee75ca114a62e0ff173886d001e468026e648bab
liuzhibo <liuzhibo@vip.qq.com>
2018-04-11 17:17:49 +0800

    更新

---
+++ week6/challenge22/analysis.py
@@ -0,0 +1,16 @@
+# 需要使用 JSON 包解析 JSON 文件
+import json
+import pandas as pd
+
+
+def analysis(file, user_id):
+    times = 0
+    minutes = 0
+
+    '''
+    补充代码：
+    1. 使用 Pandas 读取数据
+    2. 使用 Pandas 选择数据
+    '''
+
+    return times, minutes
